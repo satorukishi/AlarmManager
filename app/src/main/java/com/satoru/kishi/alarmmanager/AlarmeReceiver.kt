fbef60ea9cd72bae1e39aec41f2ad7b7cf1f66e6
logonrm <logonrm@fiap.com.br>
2018-02-17 15:13:32 -0200

    Alarm Manager e musica quando o AlarmManage é disparado

--- app/src/main/java/com/satoru/kishi/alarmmanager/AlarmeReceiver.kt
+++ app/src/main/java/com/satoru/kishi/alarmmanager/AlarmeReceiver.kt
@@ -10,7 +10,7 @@ class AlarmeReceiver : BroadcastReceiver() {
     private var mp: MediaPlayer? = null
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        mp = MediaPlayer.create(context, R.raw.windows_xp_startup)
+        mp = MediaPlayer.create(context, R.raw.windowsxp_startup)
         mp!!.start()
         Toast.makeText(context, "Alarm...", Toast.LENGTH_SHORT).show()
     }
